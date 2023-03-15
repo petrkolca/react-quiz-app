9ce809522140d9dff80d8805190a076516952d50
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-15 17:00:29 +1100

    App Context - setting states

--- src/store/app-context.jsx
+++ src/store/app-context.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+// temporary QUIZ API for app build
+const TEMP_API_URL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
 
 const AppContext = React.createContext();
 
 const AppProvider = (props) => {
+  const [isWaiting, setIsWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <AppContext.Provider value='quiz-data'>{props.children}</AppContext.Provider>
+    <AppContext.Provider value={{
+      isWaiting,
+      loading,
+      questions,
+      index,
+      correctAnswer,
+      error,
+      isModalOpen
+    }}>{props.children}</AppContext.Provider>
   )
 }
 
